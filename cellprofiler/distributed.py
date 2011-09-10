75d5525e5d3d2e7daa4db620b14e561355342bbb
Jacob Silterra <jsilter@gmail.com>
2011-09-10 01:18:46 -0400

    Removed extraneous whitespace to comply with pep8.
    A pep8 checker will find a few discrepancies, but this developer believes the readability is improved with the lack of whitespace before run_distributed and with raise RuntimeError('could not create image set list') being longer than 80 chars.

--- cellprofiler/distributed.py
+++ cellprofiler/distributed.py
@@ -18,7 +18,7 @@ import cellprofiler.cpimage as cpi
 import cellprofiler.workspace as cpw
 import cellprofiler.measurements as cpmeas
 
-# whether CP should run distributed (changed by preferences, or by command line)
+#Whether CP should run distributed (changed by preferences or command line)
 force_run_distributed = False
 def run_distributed():
     return (force_run_distributed or cpprefs.get_run_distributed())
@@ -28,13 +28,12 @@ class QueueDict(OrderedDict):
     Dictionary we can use as a queue
     """
 
-    def rotate(self,n=1):
+    def rotate(self, n=1):
         value = None
-        for stp in xrange(0,n):
-            key,value = self.popitem(last=False)
+        for stp in xrange(0, n):
+            key, value = self.popitem(last=False)
             self[key] = value
         return value
-    
 
 class Distributor(object):
     def __init__(self):
@@ -45,14 +44,17 @@ class Distributor(object):
         self.measurements = None
         self.work_queue = QueueDict()
 
-    def start_serving(self, pipeline, output_file,address = 'tcp://127.0.0.1',port = None):
+    def start_serving(self, pipeline, output_file,
+                      address='tcp://127.0.0.1', port=None):
 
         self.output_file = output_file
 
         # make sure createbatchfiles is not in the pipeline
-        if 'CreateBatchFiles' in [module.module_name for module in pipeline.modules()]:
+        mod_names = [mod.module_name for mod in pipeline.modules()]
+        if 'CreateBatchFiles' in mod_names:
             # XXX - should offer to ignore?
-            raise RuntimeError('CreateBatchFiles should not be used with distributed processing.')
+            raise RuntimeError('CreateBatchFiles should not '
+                'be used with distributed processing.')
 
         # duplicate pipeline
         pipeline = pipeline.copy()
@@ -60,10 +62,10 @@ class Distributor(object):
         # create the image list
         image_set_list = cpi.ImageSetList()
         image_set_list.combine_path_and_file = True
-        self.measurements = cpmeas.Measurements(filename = self.output_file)
-        workspace = cpw.Workspace(pipeline, None, None, None, self.measurements,
-                                  image_set_list)
-                                  
+        self.measurements = cpmeas.Measurements(filename=self.output_file)
+        workspace = cpw.Workspace(pipeline, None, None, None,
+                                  self.measurements, image_set_list)
+
         if not pipeline.prepare_run(workspace):
             raise RuntimeError('Could not create image set list for distributed processing.')
 
@@ -86,16 +88,16 @@ class Distributor(object):
         #This saves the data directly on disk, uncompressed
         raw_pipeline_path = module.save_pipeline(workspace)
         #Read it back into memory
-        raw_pipeline_file = open(raw_pipeline_path,'r')
+        raw_pipeline_file = open(raw_pipeline_path, 'r')
         pipeline_txt = raw_pipeline_file.read()
-        pipeline_fd,pipeline_path = tempfile.mkstemp()     
-        pipeline_file = open(pipeline_path,'w')
+        pipeline_fd, pipeline_path = tempfile.mkstemp()
+        pipeline_file = open(pipeline_path, 'w')
 
         pipeline_blob = zlib.compress(pipeline_txt)
         pipeline_file.write(pipeline_blob)
         pipeline_file.close()
         self.pipeline_path = 'file://%s' % (pipeline_path)
-        
+
         # we use the hash to make sure old results don't pollute new
         # ones, and that workers are fetching what they expect.
         self.pipeline_blob_hash = hashlib.sha1(pipeline_blob).hexdigest()
@@ -107,12 +109,12 @@ class Distributor(object):
 
         # start serving
         self.total_jobs = image_set_list.count()
-        
-        self.run(address,port)
-        
+
+        self.run(address, port)
+
     def has_work(self):
         return len(self.work_queue) > 0
-    
+
     def get_next(self):
         try:
             return self.work_queue.rotate()
@@ -121,19 +123,19 @@ class Distributor(object):
             return None
 
     def get_pipeline_info(self):
-        return {'path':self.pipeline_path,
-                    'hash':self.pipeline_blob_hash}
-    
-    def report_results(self,msg):
+        return {'path': self.pipeline_path,
+                    'hash': self.pipeline_blob_hash}
+
+    def report_results(self, msg):
         jobnum = msg['jobnum']
         pipeline_hash = msg['pipeline_hash']
-        work_item = self.work_queue.get(jobnum,None)
-        response = {'status':'failure'}
+        work_item = self.work_queue.get(jobnum, None)
+        response = {'status': 'failure'}
         if(work_item is None):
             resp = 'work item %s not found' % (jobnum)
             print resp
             response['code'] = resp
-        elif(pipeline_hash != work_item): 
+        elif(pipeline_hash != work_item):
             # out of date result?
             resp = "ignored mismatched pipeline hash", pipeline_hash, work_item
             response['code'] = resp
@@ -147,7 +149,7 @@ class Distributor(object):
             temp_hdf5.flush()
 
             curr_meas = cpmeas.load_measurements(filename=temp_hdf5.name)
-            self.measurements.combine_measurements(curr_meas,can_overwrite = True)
+            self.measurements.combine_measurements(curr_meas, can_overwrite=True)
             del curr_meas
 
             #This can potentially cause a race condition,
@@ -155,28 +157,27 @@ class Distributor(object):
             #as the server.
             del self.work_queue[jobnum]
             self.jobs_finished += 1
-            response = {'status':'success','remaining': len(self.work_queue)}
+            response = {'status': 'success', 'remaining': len(self.work_queue)}
         return response
-    
-        
-    def run(self,address,port):
+
+    def run(self, address, port):
         context = zmq.Context()
         sender = context.socket(zmq.REP)
         if(port is not None):
-            self.server_URL = "%s:%s" % (address,port)
+            self.server_URL = "%s:%s" % (address, port)
             sender.bind(self.server_URL)
         else:
             port = sender.bind_to_random_port(address)
-            self.server_URL = "%s:%s" % (address,port)
-        
+            self.server_URL = "%s:%s" % (address, port)
+
         self.jobs_finished = 0
-        
+
         running = True
         while running:
             #XXX Implement a timeout
             raw_msg = sender.rcv()
             msg = json.loads(raw_msg)
-            response = {'status':'bad request'}
+            response = {'status': 'bad request'}
             if('type' not in msg):
                 #TODO Log something
                 continue
@@ -185,23 +186,24 @@ class Distributor(object):
             if(msg['type'] == 'next_job'):
                 job = self.get_next()
                 if(job is None):
-                    job = {'status':'nowork'}
+                    job = {'status': 'nowork'}
                 response = job
-            elif((msg['type'] == 'result') and (msg.has_key('result')) ):
+            elif((msg['type'] == 'result') and ('result' in msg)):
                 response = self.report_results(msg)
             elif(msg['type'] == 'stop'):
-                #Huge security risk; anybody can just tell the server to stop?
+                #Something of a security risk.
+                #Anybody can just tell the server to stop?
                 #While we're on the subject, this may not be a secure way
-                #of passing data
-                response = {'status':'stopping'}
-                
+                #of passing data. Maybe implement crypto signatures?
+                response = {'status': 'stopping'}
+
             sender.send(json.dumps(response))
-            
+
             running &= self.has_work()
-            
+
         sender.close()
         context.term()
-        
+
         self.stop_serving()
 
     def stop_serving(self):
@@ -220,7 +222,7 @@ class JobInfo(object):
         self.context = None
         self.socket = None
         self.connected = False
-        
+
     def _init_connection(self):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
@@ -231,26 +233,26 @@ class JobInfo(object):
         # fetch the pipeline
         self._get_pipeline_blob()
         self.pipeline_hash = hashlib.sha1(self.pipeline_blob).hexdigest()
-        
+
         if(not self.connected):
             self._init_connection()
-        self.socket.send(json.dumps({'type':'next_job'}))
+        self.socket.send(json.dumps({'type': 'next_job'}))
         raw_msg = self.socket.recv()
         msg = json.loads(raw_msg)
-        if msg.get('status','').lower() == 'nowork':
+        if msg.get('status', '').lower() == 'nowork':
             assert False, "No work to be had..."
         self.job_num = msg.keys()[0]
         pipeline_hash = msg[self.job_num]
         image_num = self.job_num
         self.image_set_start = int(image_num)
         self.image_set_end = int(image_num)
-        
+
         assert pipeline_hash == self.pipeline_hash, "Mismatched hash, probably out of sync with server"
 
     def _get_pipeline_blob(self):
         if(not self.connected):
             self._init_connection()
-        self.socket.send({'type':'pipeline_path'})
+        self.socket.send({'type': 'pipeline_path'})
         raw_msg = self.socket.recv()
         msg = json.loads(raw_msg)
         pipeline_path = msg['path']
@@ -262,14 +264,14 @@ class JobInfo(object):
         return StringIO.StringIO(zlib.decompress(self.pipeline_blob))
 
     def report_measurements(self, pipeline, measurements):
-        meas_file = open(measurements.hdf5_dict.filename,'r+b')
+        meas_file = open(measurements.hdf5_dict.filename, 'r+b')
         out_measurements = meas_file.read()
         if(not self.connected):
             self._init_connection()
-        msg = {'type':'results','result':out_measurements}
+        msg = {'type': 'results', 'result': out_measurements}
         raw_msg = json.dumps(msg)
         self.socket.send(raw_msg)
-        
+
 
 def fetch_work(base_URL):
     jobinfo = JobInfo(base_URL)
