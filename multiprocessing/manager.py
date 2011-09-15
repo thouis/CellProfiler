6e55990ee523bd6ba77f83fb7a89512486ac3393
Thouis (Ray) Jones <thouis@gmail.com>
2011-09-15 16:43:35 +0200

    make gui and manager into classes, add stub worker

--- multiprocessing/manager.py
+++ multiprocessing/manager.py
@@ -2,22 +2,98 @@ import os
 import time
 
 import zmq
+from zmq.eventloop import ioloop, zmqstream
+import multiprocessing
 
-def manager(q):
-    # get the GUI's address
-    commands_port, feedback_port = q.get()
-    print "Manager binding to %s and %s" % (commands_port, feedback_port)
+import worker
 
-    # set up ZMQ sockets
-    context = zmq.Context()
-    gui_commands = context.socket(zmq.PULL)
-    gui_commands.connect("tcp://127.0.0.1:%d" % commands_port)
-    gui_feedback = context.socket(zmq.PUSH)
-    gui_feedback.connect("tcp://127.0.0.1:%d" % feedback_port)
+class Manager(object):
+    def __init__(self, commands_port, feedback_port):
+        # set up ZMQ sockets
+        context = zmq.Context()
 
-    # for now, echo twice
-    while True:
-        msg = gui_commands.recv()
-        gui_feedback.send(msg)
-        time.sleep(1)
-        gui_feedback.send(msg)
+        # Connect to GUI
+        print "Manager connecting to %s and %s" % (commands_port, feedback_port)
+        self.gui_commands = context.socket(zmq.PULL)
+        self.gui_commands.connect("tcp://127.0.0.1:%d" % commands_port)
+        self.gui_feedback = context.socket(zmq.PUSH)
+        self.gui_feedback.connect("tcp://127.0.0.1:%d" % feedback_port)
+
+        # Create worker-facing sockets
+        def socket_random_port(socket_type):
+            s = context.socket(socket_type)
+            p = s.bind_to_random_port('tcp://127.0.0.1')
+            return s, p
+        self.jobs, jobs_port = socket_random_port(zmq.ROUTER)
+        self.results, results_port = socket_random_port(zmq.PULL)
+        self.control, control_port = socket_random_port(zmq.PUB)
+        self.exceptions, exceptions_port = socket_random_port(zmq.ROUTER)
+
+        # start workers - XXX move to separate function to handle restarting when needed
+        try:
+            count = multiprocessing.cpu_count()
+        except NotImplementedError:
+            count = 2
+        workers = [multiprocessing.Process(target=worker.worker, args=(jobs_port, results_port, control_port, exceptions_port)) for n in range(count)]
+        for w in workers:
+            w.daemon = True  # autokill when parent exits
+            w.start()
+
+        # setup the handlers - use ZMQStreams because we want to call
+        # the hander for every message.  see
+        # http://lists.zeromq.org/pipermail/zeromq-dev/2011-May/011509.html
+        self.loop = loop = ioloop.IOLoop()
+        for socket, handler in zip([self.gui_commands, self.jobs, self.results, self.exceptions],
+                                   [self.on_gui_command, self.on_job_request, self.on_results, self.on_exceptions]):
+            zmqstream.ZMQStream(socket, loop).on_recv(handler)
+
+        # Keep workers alive.  They die if starved more than a few minutes
+        ioloop.PeriodicCallback(self.keep_alive, 5 * 1000, loop).start()
+
+        # workers waiting
+        self.workers_waiting = []
+
+        # jobs waiting
+        self.jobs_waiting = []
+
+        # serve
+        loop.start()
+
+    def on_gui_command(self, msg):
+        print "GUI COMMAND", msg
+        if msg[0] == 'quit':
+            self.loop.stop()
+        elif msg[0] == 'interrupt':
+            self.control.send(interrupt, flags=zmq.NOBLOCK)
+        else:
+            # number of jobs to queue
+            for i in range(int(msg[0])):
+                self.jobs_waiting.append("%d" % (i + 1))
+            self.feed_workers()
+
+    def on_job_request(self, msg):
+        print "JOB REQ", msg
+        self.workers_waiting.append(msg[-2])
+        self.feed_workers()
+
+    def feed_workers(self):
+        while self.workers_waiting and self.jobs_waiting:
+            worker = self.workers_waiting.pop(0)
+            job = self.jobs_waiting.pop(0)
+            self.jobs.send_multipart(worker + [job])
+            self.gui_feedback.send("sent job %s to %s" % (job, worker))
+
+    def on_results(self, msg):
+        print "RESULTS", msg
+
+    def on_exceptions(self, msg):
+        print "EXCEPTION", msg
+
+    def keep_alive(self):
+        print "KEEP ALIVE", leb(self.workers_waiting)
+        for worker in self.workers_waiting:
+            self.jobs.send_multipart(worker + ['0'])
+        self.workers_waiting = []
+
+def manager(commands_port, feedback_port):
+    m = Manager(commands_port, feedback_port)  # it just runs
--- multiprocessing/test_gui.py
+++ multiprocessing/test_gui.py
@@ -11,86 +11,93 @@ import multiprocessing
 
 import manager
 
-def read_commands():
-    # we do this in a separate thread to allow asynchronous output
-    try:
+class TestGUI(object):
+    def __init__(self):
+        # zmq setup
+        context = zmq.Context()
+        self.gui_commands = context.socket(zmq.PUSH)
+        self.gui_feedback = context.socket(zmq.PULL)
+        commands_port = self.gui_commands.bind_to_random_port("tcp://127.0.0.1")
+        feedback_port = self.gui_feedback.bind_to_random_port("tcp://127.0.0.1")
+
+        # start manager
+        self.m = multiprocessing.Process(target=manager.manager, args=(commands_port, feedback_port))
+        self.m.start()
+
+        # pipe for command input
+        self.commands_pipe_r, self.commands_pipe_w = os.pipe()
+
+    def run(self):
+        input_thread = threading.Thread(target=self.read_commands)
+        output_thread = threading.Thread(target=self.write_output)
+        commands_thread = threading.Thread(target=self.process_commands)
+        input_thread.daemon = True  # we exit when the commands thread does
+        output_thread.daemon = True
+        input_thread.start()
+        output_thread.start()
+        commands_thread.start()
+
+        # run until commands thread ends
+        commands_thread.join()
+        self.m.terminate()
+
+    def read_commands(self):
+        # we do this in a separate thread to allow asynchronous output
+        try:
+            while True:
+                os.write(self.commands_pipe_w, raw_input("> "))
+        except:
+            os.write(self.commands_pipe_w, "quit")
+
+    def write_output(self):
         while True:
-            os.write(commands_pipe_w, raw_input("> "))
-    except:
-        os.write(commands_pipe_w, "quit")
+            # wait for feedback
+            msg = self.gui_feedback.recv()
+            # overwrite current typing
+            sys.stdout.write("\r  " + (' ' * len(readline.get_line_buffer())))
+            # write line
+            sys.stdout.write("\rMSG from Manager: >%s<\n" % msg)
+            # redisplay prompt
+            readline.redisplay()
 
-def write_output():
-    while True:
-        # wait for feedback
-        msg = gui_feedback.recv()
-        # overwrite current typing
-        sys.stdout.write("\r  " + (' ' * len(readline.get_line_buffer())))
-        # write line
-        sys.stdout.write("\rMSG from Manager: >%s<\n" % msg)
-        # redisplay prompt
-        readline.redisplay()
+    def process_commands(self):
+        while True:
+            command = os.read(self.commands_pipe_r, 1024)
+            # commands are:
+            # N - submit N jobs
+            # interrupt - stop current jobs
+            # quit - kill subprocesses and exit
+            if command == 'quit':
+                self.send_quit()
+                time.sleep(1)
+                return
+            elif command == 'interrupt':
+                self.send_interrupt()
+            else:
+                try:
+                    n = int(command)
+                    self.send_jobs(n)
+                except:
+                    print "bad command", command
+                    pass
 
-def process_commands():
-    while True:
-        command = os.read(commands_pipe_r, 1024)
-        # commands are:
-        # N - submit N jobs
-        # interrupt - stop current jobs
-        # quit - kill subprocesses and exit
-        if command == 'quit':
-            send_quit()
-            return
-        elif command == 'interrupt':
-            send_interrupt()
-        else:
-            try:
-                n = int(command)
-                send_jobs(n)
-            except:
-                print "bad command", command
-                pass
+    def send_quit(self):
+        try:
+            self.gui_commands.send("quit", flags=zmq.NOBLOCK)
+        except:
+            pass  # couldn't send quit, probably because the manager went away
 
-def send_quit():
-    gui_commands.send("quit")
+    def send_interrupt(self):
+        try:
+            self.gui_commands.send("interrupt", flags=zmq.NOBLOCK)
+        except:
+            pass  # XXX - server probably died
 
-def send_interrupt():
-    gui_commands.send("interrupt")
-
-def send_jobs(n):
-    gui_commands.send("%d" % n)
-
+    def send_jobs(self, n):
+        try:
+            self.gui_commands.send("%d" % n, flags=zmq.NOBLOCK)
+        except:
+            pass  # XXX - server probably died
 
 if __name__ == '__main__':
-    # Queue to communicate to manager
-    q = multiprocessing.Queue()
-
-    # start manager
-    m = multiprocessing.Process(target=manager.manager, args=(q,))
-    m.daemon = True  # autokill when parent exits
-    m.start()
-
-    # zmq setup
-    context = zmq.Context()
-    gui_commands = context.socket(zmq.PUSH)
-    commands_port = gui_commands.bind_to_random_port("tcp://127.0.0.1")
-    gui_feedback = context.socket(zmq.PULL)
-    feedback_port = gui_feedback.bind_to_random_port("tcp://127.0.0.1")
-
-    # tell the Manager where to bind, then hangup
-    print "GUI ports:", commands_port, feedback_port
-    q.put((commands_port, feedback_port))
-
-    # pipe for command input
-    commands_pipe_r, commands_pipe_w = os.pipe()
-
-    input_thread = threading.Thread(target=read_commands)
-    output_thread = threading.Thread(target=write_output)
-    commands_thread = threading.Thread(target=process_commands)
-    input_thread.daemon = True  # we exit when the commands thread does
-    output_thread.daemon = True
-    input_thread.start()
-    output_thread.start()
-    commands_thread.start()
-
-    # run until commands thread ends
-    commands_thread.join()
+    TestGUI().run()
