178f049a0cf4f12684d324a7df3e80e985dfa815
Thouis (Ray) Jones <thouis@gmail.com>
2011-10-12 12:13:52 +0200

    filtering works, focused clause udpates list

--- cellprofiler/gui/fileselector2.py
+++ cellprofiler/gui/fileselector2.py
@@ -45,22 +45,30 @@ class Clause(wx.Panel):
         self.metadata_col = wx.Choice(self, choices=['Pathname', 'Filename'])
         self.filter_choice = wx.Choice(self, choices=self.FILTER_CHOICES.keys())
         self.value = wx.TextCtrl(self, value='', style=wx.TE_PROCESS_ENTER)
-        cross_mark = wx.ArtProvider.GetBitmap(wx.ART_CROSS_MARK, wx.ART_MENU)
-        remove_button = wx.Button(self, label='x', style=wx.BU_EXACTFIT)
+        add_button = wx.Button(self, label='+', style=wx.BU_EXACTFIT)
+        remove_button = self.remove_button = wx.Button(self, label='-', style=wx.BU_EXACTFIT)
 
         self.sizer.Add(self.metadata_col)
         self.sizer.AddSpacer(2)
         self.sizer.Add(self.filter_choice)
         self.sizer.AddSpacer(2)
         self.sizer.Add(self.value, proportion=1)
+        self.sizer.AddSpacer(2)
+        self.sizer.Add(add_button)
         self.sizer.AddSpacer(1)
         self.sizer.Add(remove_button)
-        self.SetSizer(self.sizer)
+
+        # add a border to show highlighting of active clause
+        border_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        border_sizer.Add(self.sizer, proportion=1, flag=wx.ALL | wx.EXPAND, border=2)
+        self.SetSizer(border_sizer)
 
         self.metadata_col.Bind(wx.EVT_CHOICE, self.update)
         self.filter_choice.Bind(wx.EVT_CHOICE, self.update)
         self.value.Bind(wx.EVT_TEXT_ENTER, self.update)
         self.value.Bind(wx.EVT_SET_FOCUS, self.set_focus)
+        self.value.Bind(wx.EVT_KILL_FOCUS, self.lose_focus)
+        add_button.Bind(wx.EVT_BUTTON, self.add)
         remove_button.Bind(wx.EVT_BUTTON, self.remove)
 
     def update(self, evt):
@@ -71,10 +79,21 @@ class Clause(wx.Panel):
         self.Refresh()
 
     def set_focus(self, evt):
+        self.BackgroundColour = '#ff7777'
         self.Parent.set_focus(self)
+
+    def lose_focus(self, evt):
+        self.BackgroundColour = wx.SystemSettings.GetColour(wx.SYS_COLOUR_BACKGROUND)
+
+    def add(self, evt):
+        self.Parent.add_after_clause(self)
 
     def remove(self, evt):
         self.Parent.remove_clause(self)
+
+    def show_remove_button(self, show):
+        self.sizer.Show(self.remove_button, show)
+        self.Layout()
 
     def filter(self, filelist):
         filter_info = self.FILTER_CHOICES[self.filter_choice.StringSelection]
@@ -92,30 +111,32 @@ class Filter(wx.Panel):
         self._clauses = []
 
         box = wx.StaticBox(self, label=name)
-        box.BackgroundColour = 'blue'
 
         self.sizer = wx.StaticBoxSizer(box, wx.VERTICAL)
         self.button_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        add_button = wx.Button(self, label='Add clause')
         channels_button = wx.Button(self, label='Define channels')
         sort_text = wx.StaticText(self, label='Order by:')
         self.sort_choice = wx.Choice(self, choices=['Pathname', 'Filename'])
         self.button_sizer.AddSpacer(10)
-        self.button_sizer.Add(add_button)
-        self.button_sizer.AddSpacer(5)
         self.button_sizer.Add(channels_button)
         self.button_sizer.AddStretchSpacer()
-        self.button_sizer.Add(sort_text)
+        self.button_sizer.Add(sort_text, flag=wx.ALIGN_BOTTOM)
         self.button_sizer.Add(self.sort_choice)
         self.sizer.Add(self.button_sizer, proportion=1, flag=wx.EXPAND)
         self.SetSizer(self.sizer)
 
-        add_button.Bind(wx.EVT_BUTTON, self.add_clause)
+        self.add_after_clause(None)  # insert a single clause
 
-    def add_clause(self, evt):
+    def add_after_clause(self, clause):
         new_clause = Clause(self)
-        self._clauses.append(new_clause)
-        self.sizer.Insert(len(self.sizer.Children) - 1, new_clause, flag=wx.EXPAND | wx.ALL, border=2)
+        if len(self._clauses) > 0:
+            new_idx = self._clauses.index(clause) + 1
+        else:
+            new_idx = 0
+        self._clauses.insert(new_idx, new_clause)
+        self.sizer.Insert(new_idx, new_clause, flag=wx.EXPAND | wx.ALL, border=2)
+        for clause in self._clauses:
+            clause.show_remove_button(len(self._clauses) > 1)
         self.Parent.Layout()
         self.Parent.Refresh()
 
@@ -123,22 +144,22 @@ class Filter(wx.Panel):
         self._clauses.remove(clause)
         self.sizer.Remove(clause)
         clause.Destroy()
+        for clause in self._clauses:
+            clause.show_remove_button(len(self._clauses) > 1)
         self.Parent.Layout()
         self.Parent.Refresh()
 
     def set_focus(self, focused_clause):
-        start_time = time.time()
         file_list = self.Parent.get_unfiltered_files()
         for clause in self._clauses:
             file_list = clause.filter(file_list)
             if clause == focused_clause:
                 break
-        self.Parent.set_filtered_list(file_list, self, focused_clause)
+        self.Parent.set_filtered_list(file_list, 'After %s, clause %d' % (self.name, self._clauses.index(focused_clause) + 1))
 
 class FilterSet(wx.Panel):
     def __init__(self, parent, name):
         wx.Panel.__init__(self, parent)
-        self.BackgroundColour = 'red'
         self.name = name
         self._filters = []
 
@@ -159,8 +180,8 @@ class FilterSet(wx.Panel):
     def get_unfiltered_files(self):
         return self.Parent.get_unfiltered_files()
 
-    def set_filtered_list(self, filtered_list, a, b):
-        self.Parent.set_filtered_list(filtered_list)
+    def set_filtered_list(self, filtered_list, feedback):
+        self.Parent.set_filtered_list(filtered_list, feedback)
 
 class CPImageSetBuilder(wx.Frame):
     def __init__(self, *args, **kwargs):
@@ -196,7 +217,7 @@ class CPImageSetBuilder(wx.Frame):
 
         self.Layout()
 
-    def update_files(self, new_list=None):
+    def update_files(self, title='Default list', new_list=None):
         def genfiles():
             for path, dirs, files in os.walk('.'):
                 for f in files:
@@ -204,14 +225,19 @@ class CPImageSetBuilder(wx.Frame):
         if new_list is None:
             self._files = [(p, f) for (p, f) in genfiles()]
             new_list = self._files
-        self.files.SetPage('<br>\n'.join(sorted([f for p, f in new_list])))
+        page = '<b>%s</b>\n' % (title)
+        page += '<table border=1 cellspacing=0 cellpadding=2 rules=cols width="100%">\n'
+        page += '<tr align=left><th><b>Filename</b></th><th><b>Pathname</b></th></tr>\n'
+        page += '\n'.join(sorted(['<tr><td>%s</td><td>%s</td></tr>' % (f, p) for p, f in new_list]))
+        page += '</table>'
+        self.files.SetPage(page)
         self.files.Refresh()
 
     def get_unfiltered_files(self):
         return self._files
 
-    def set_filtered_list(self, new_list):
-        self.update_files(new_list)
+    def set_filtered_list(self, new_list, title):
+        self.update_files(title, new_list)
 
 class MyApp(wx.App):
     def OnInit(self):
